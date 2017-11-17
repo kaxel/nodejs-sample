952834e8226174dd27c36a532906a0433d787ec7
Krister Axel <ka@ashland.io>
2017-11-17 14:06:06 -0800

    added a get request and wired the controller

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var listController = require('./controllers/listController');
+
+var app = express();
+
+//set up the template engine
+app.set('view engine', 'ejs'); 
+
+//set up  static files
+app.use(express.static('./public'));
+
+//fire controller
+listController(app);
+
+//port listener
+app.listen(3000);
+console.log('You are listening to the sound of port 3000');
+
+// USING MVC
---
+++ controllers/listController.js
@@ -0,0 +1,16 @@
+module.exports = function(app){
+	
+	app.get('/list', function(req, res){
+		res.render('list');
+	});
+	
+	app.post('/list', function(req, res){
+		
+	});
+	
+	app.delete('/list', function(req, res){
+		
+	});
+	
+	
+};
